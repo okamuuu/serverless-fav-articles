60d416141a66133d0d929a1b308b0f1677acc477
okamuuu <okamuuu@gmail.com>
2018-04-10 12:02:53 +0900

    react-router

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link, NavLink, withRouter } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Articles from './Articles'
 
-export default App;
+const Header = ({onClick}) => (
+  <h1 className="text-center" style={{cursor: "pointer" }} onClick={onClick}>Favorite Articles</h1>
+)
+
+const Nav = () => (
+  <nav className="nav nav-pills">
+    <NavLink className="nav-link" exact to="/articles">Articles</NavLink>
+    <NavLink className="nav-link" exact to="/articles/favorite">Favorite Articles</NavLink>
+  </nav>
+)
+
+const Footer = () => (<p className="text-center">Favorite Articles</p>)
+
+const Routes = withRouter(({history}) => (
+  <div className="container">
+    <Header onClick={() => history.push("/")} />
+    <Nav />
+    <Switch>
+      <Route exact path="/" component={Articles.List}/>
+      <Route exact path="/articles" component={Articles.List}/>
+      <Route exact path="/articles/favorite" component={Articles.FavoriteList}/>
+      <Route exact path="/articles/:id" component={Articles.Show}/>
+    </Switch>
+    <Footer />
+  </div>
+))
+
+const App = () => (
+  <Router>
+    <Routes />
+  </Router>
+)
+
+export default App
